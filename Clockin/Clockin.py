f56ac80119cb4a9e90eae95c9f8fff0278a518cc
B1ue1nWh1te <708968728@qq.com>
2021-08-14 16:01:35 +0800

    更新打卡脚本

--- Clockin/Clockin.py
+++ Clockin/Clockin.py
@@ -1,25 +1,11 @@
 from PIL import Image
 from io import BytesIO
 import pytesseract
-
 import requests
-import Pusher
 import json
 import time
 import sys
 import os
-
-
-Sum = ""  # 全校打卡人数
-Now = ""  # 任务状态：打卡/检查打卡情况
-LogFile = ""  # 日志文件对象
-AllAccount = []  # 队列中所有账户的数据
-RetryAccount = []  # 队列中要重试的所有账户的数据
-Headers = {
-    'Host': 'fangkong.hnu.edu.cn',  # 构造来源链接
-    'Content-Type': 'application/json',  # 重要
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-}
 
 
 # 日志
@@ -29,6 +15,7 @@ def Log(Text):
         Text = "{}  {}".format(Time, Text)
         print(Text)
         LogFile.write(Text + "\n")
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Time = time.strftime("%H:%M:%S")
@@ -36,17 +23,41 @@ def Log(Text):
         print(Text)
 
 
+# 推送至企业微信
+def PushToEnterpriseWechat(Message="", Title="自动打卡"):
+    try:
+        Log(Message)
+        global CorpID, CorpSecret, AgentID, Manager
+        Key = {"corpid": CorpID, "corpsecret": CorpSecret}
+        AccessToken = {"access_token": requests.get(TokenApi, params=Key, timeout=3).json()['access_token']}
+        Time = time.strftime("%m{}%d{} %H:%M:%S").format('月', '日')
+        Message = "[{}]{}\n{}".format(Title, Time, Message)
+        Data = {
+            "touser": Manager,
+            "msgtype": "text",
+            "agentid": AgentID,
+            "text": {
+                "content": Message
+            }
+        }
+        Data = json.dumps(Data)  # 将json转换为str
+        PostResponse = requests.post(PushApi, params=AccessToken, data=Data, timeout=3)
+
+    except Exception:
+        ExceptionInformation = sys.exc_info()
+        Text = "[推送异常]异常信息为:{}".format(ExceptionInformation)
+        Log(Text)
+
+
 # 解验证码
 def Devercode():
     try:
         GetVercodeUrl = "https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode"
-        VercodeToken = requests.get(GetVercodeUrl, headers=Headers).json()[
-            "data"]["Token"]
+        VercodeToken = requests.get(GetVercodeUrl, headers=Headers).json()["data"]["Token"]
         VercodeUrl = "https://fangkong.hnu.edu.cn/imagevcode?token=" + VercodeToken
         VercodeResponse = requests.get(VercodeUrl, headers=Headers)
         VercodeImage = Image.open(BytesIO(VercodeResponse.content))
-        Vercode = pytesseract.image_to_string(
-            VercodeImage, config="--psm 7 vercode")  # 命令解释：指定页面分段模式 将图片视作单行文本
+        Vercode = pytesseract.image_to_string(VercodeImage, config="--psm 7 vercode")  # 命令解释：指定页面分段模式 将图片视作单行文本
         if Vercode != "":
             Vercode = Vercode.replace("\n", "")
             Vercode = Vercode.replace(" ", "")
@@ -57,6 +68,7 @@ def Devercode():
                 return Devercode()
         else:
             return Devercode()
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Text = "[解码异常]异常信息为:{}".format(ExceptionInformation)
@@ -91,8 +103,7 @@ def Login(Username, Password):
         elif Content["msg"] == "账号或密码错误":
             return ["账号或密码错误"]
         elif Content["msg"] == "成功":
-            CookiesDict = requests.utils.dict_from_cookiejar(
-                LoginResponse.cookies)
+            CookiesDict = requests.utils.dict_from_cookiejar(LoginResponse.cookies)
             Time = str(int(time.time()))
             LoginedHeaders = {
                 'Host': 'fangkong.hnu.edu.cn',
@@ -105,34 +116,29 @@ def Login(Username, Password):
             return LoginedHeaders
         else:
             return ["其他异常", Content["msg"]]
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Text = "[登录异常]异常信息为:{}".format(ExceptionInformation)
         Log(Text)
-        Status = ["登录异常"]
-        return Status
+        return ["登录异常"]
 
 
 # 获取信息
 def GetInformation(LoginedHeaders):
     try:
-        global Sum
         PersonalInfomationUrl = "https://fangkong.hnu.edu.cn/api/v1/clockinlog/isclockinloginfo"
-        PersonalDict = requests.get(
-            PersonalInfomationUrl, headers=LoginedHeaders).json()
+        PersonalDict = requests.get(PersonalInfomationUrl, headers=LoginedHeaders).json()
         Name = PersonalDict["data"]["Name"]
         ClassName = PersonalDict["data"]["ClassName"]
         Status = PersonalDict["msg"]
-        BaseInfomationUrl = "https://fangkong.hnu.edu.cn/api/v1/clockincount/getcountlist"
-        Sum = str(requests.get(BaseInfomationUrl,
-                  headers=LoginedHeaders).json()["data"]["AllCount"])
         Information = {
-            "全校已打卡人数": Sum,
             "姓名": Name,
             "班级": ClassName,
             "打卡状态": Status
         }
         return Information
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Text = "[获取信息异常]异常信息为:{}".format(ExceptionInformation)
@@ -199,12 +205,12 @@ def ClockIn(LoginedHeaders, ClockinInformation):
             "Latitude": Latitude
         }
         Data = json.dumps(message)
-        ClockInDict = requests.post(
-            ClockInUrl, headers=LoginedHeaders, data=Data).json()
+        ClockInDict = requests.post(ClockInUrl, headers=LoginedHeaders, data=Data).json()
         if ClockInDict["msg"] == "成功":
             return ["成功"]
         else:
             return ["其他异常", ClockInDict["msg"]]
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Text = "[打卡异常]异常信息为:{}".format(ExceptionInformation)
@@ -214,6 +220,7 @@ def ClockIn(LoginedHeaders, ClockinInformation):
 
 # 打卡流水线
 def ClockLine(Account):
+    global RetryAccount
     Username = Account[0]
     Password = Account[1]
     ClockinInformation = Account[2].split("-")
@@ -221,113 +228,89 @@ def ClockLine(Account):
     Status = list(LoginedHeaders)[0]
     if (Status == "账号或密码错误"):
         Message = "[登录失败] [{}]\n账号与密码不匹配\n警告:请手动打卡!".format(Username)
-        Log(Message)
-        Pusher.PushToEnterpriseWechat("text", "manager", Message, Title="自动打卡")
+        PushToEnterpriseWechat(Message)
     elif (Status == "登录异常"):
         Message = "[登录失败] [{}]在登录时出现程序异常".format(Username)
         RetryAccount.append(Account)
-        Log(Message)
-        Pusher.PushToEnterpriseWechat("text", "manager", Message, Title="自动打卡")
+        PushToEnterpriseWechat(Message)
     elif (Status == "其他异常"):
-        Message = "[登录失败] [{}]\n在登录时出现其他问题\n提示为:{}".format(
-            Username, list(LoginedHeaders)[1])
+        Message = "[登录失败] [{}]\n在登录时出现其他问题\n提示为:{}".format(Username, list(LoginedHeaders)[1])
         RetryAccount.append(Account)
-        Log(Message)
-        Pusher.PushToEnterpriseWechat("text", "manager", Message, Title="自动打卡")
+        PushToEnterpriseWechat(Message)
     elif (Status == "解码异常"):
         Message = "[登录失败] [{}]在登录时解码异常"
         RetryAccount.append(Account)
-        Log(Message)
-        Pusher.PushToEnterpriseWechat("text", "manager", Message, Title="自动打卡")
+        PushToEnterpriseWechat(Message)
     else:
         Information = GetInformation(LoginedHeaders)
         if (list(Information)[0] == "获取信息异常"):
             Message = "[获取信息异常] [{}]\n在获取信息时出现程序异常".format(Username)
             RetryAccount.append(Account)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
-            return 0
-        District = ClockinInformation[0] + \
-            ClockinInformation[1] + ClockinInformation[2]
+            PushToEnterpriseWechat(Message)
+            return 1
+        District = ClockinInformation[0] + ClockinInformation[1] + ClockinInformation[2]
         if (Information["打卡状态"] == "今日已打卡"):
-            Message = "[今日已打] [{}]\n[姓名]{} [班级]{}\n[全校已打卡人数]{}\n[地区]{}".format(Username,
-                                                                               Information["姓名"], Information["班级"], Information["全校已打卡人数"], District)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
-            return 0
+            Message = "[今日已打] [{}]\n[姓名]{} [班级]{}\n[地区]{}".format(Username, Information["姓名"], Information["班级"], District)
+            PushToEnterpriseWechat(Message)
+            return 1
         Log("[{}]正在尝试打卡".format(Username))
         Status = ClockIn(LoginedHeaders, ClockinInformation)
         if (Status[0] == "成功"):
-            CInformation = "[打卡信息]\n地区:{}\n地址:{}\n经度:{} E\n纬度:{} N\n早晚体温:{}℃".format(
-                District, ClockinInformation[3], ClockinInformation[4], ClockinInformation[5], ClockinInformation[6])
-            Message = "[打卡成功] [{}]\n[姓名]{} [班级]{}\n[全校已打卡人数]{}\n{}".format(
-                Username, Information["姓名"], Information["班级"], Information["全校已打卡人数"], CInformation)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
+            CInformation = "[打卡信息]\n地区:{}\n早晚体温:{}℃".format(District, ClockinInformation[6])
+            Message = "[打卡成功] [{}]\n[姓名]{} [班级]{}\n{}".format(Username, Information["姓名"], Information["班级"], CInformation)
+            PushToEnterpriseWechat(Message)
         elif (Status[0] == "打卡异常"):
             Message = "[打卡失败] [{}]在打卡时出现程序异常".format(Username)
             RetryAccount.append(Account)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
+            PushToEnterpriseWechat(Message)
         elif (Status[0] == "其他异常"):
-            Message = "[打卡失败] [{}]在打卡时出现其他问题\n提示为:{}".format(
-                Username, Status[1])
+            Message = "[打卡失败] [{}]在打卡时出现其他问题\n提示为:{}".format(Username, Status[1])
             RetryAccount.append(Account)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
+            PushToEnterpriseWechat(Message)
 
 
 # 运行开始
 def Start():
     try:
-        global Now, LogFile, AllAccount
+        # 初始化
+        global LogFile, AllAccount
+
+        # 日志
+        if not os.path.exists(os.getcwd() + "\\Log"):
+            os.mkdir("Log")
+        Course = f"{os.getcwd()}\\Log\\{time.strftime('%Y%m%d')}.txt"
+        LogFile = open(Course, "a+", encoding="utf-8")
+
+        # 加载数据数据
         with open("Data.txt", "r", encoding="utf-8") as DataBase:
             for line in DataBase:
                 line = line.strip()
                 List = line.split("---")
                 AllAccount.append(List)
-        if not os.path.exists(os.getcwd() + "\\Log"):
-            os.mkdir("Log")
-        Course = f"{os.getcwd()}\\Log\\{time.strftime('%Y%m%d')}.txt"
-        if os.path.exists(Course):
-            Now = "检查"
-        else:
-            Now = "打卡"
-        LogFile = open(Course, "a+", encoding="utf-8")
+
         ClockMain()
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Message = "[运行异常]警告：开始函数运行异常已退出。异常信息：{}".format(ExceptionInformation)
-        Pusher.PushToEnterpriseWechat("text", "manager", Message, Title="自动打卡")
-        return 0
+        PushToEnterpriseWechat(Message)
+        sys.exit(1)
 
 
 # 运行即将结束
 def Finish():
     try:
-        if (Now == "打卡"):
-            Message = "[打卡已完成]\n[目前全校已打卡人数]{}".format(Sum)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
-        else:
-            Message = "[检查已完成]\n[目前全校已打卡人数]{}".format(Sum)
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
+        Message = "打卡已完成"
+        PushToEnterpriseWechat(Message)
         Log("程序正常结束\n" + "-" * 50)
         LogFile.close()
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Text = "程序未正常结束\n异常信息为:{}".format(ExceptionInformation)
         Log(Text)
         LogFile.close()
-        return 0
+        sys.exit(1)
 
 
 # 主函数
@@ -338,41 +321,53 @@ def ClockMain():
         Try = requests.get(Url, headers=Headers, timeout=30)
         Code = Try.status_code
         if (Code != 200):
-            Message = "[连接失败]\n目标页面异常，将在3分钟后再次尝试连接"
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
+            Message = "[连接失败]将在3分钟后再次尝试连接"
+            PushToEnterpriseWechat(Message)
             time.sleep(180)
             Try = requests.get(Url, headers=Headers, timeout=30)
             Code = Try.status_code
             if (Code == 200):
                 Message = "[重连成功]"
-                Log(Message)
-                Pusher.PushToEnterpriseWechat(
-                    "text", "manager", Message, Title="自动打卡")
+                PushToEnterpriseWechat(Message)
             else:
                 Message = "[连接失败]警告：主函数已退出。"
-                Log(Message)
-                Pusher.PushToEnterpriseWechat(
-                    "text", "manager", Message, Title="自动打卡")
-                return 0
+                PushToEnterpriseWechat(Message)
+                sys.exit(1)
+
         for Account in AllAccount:
             ClockLine(Account)
         if (len(RetryAccount) != 0):
             Message = "即将对之前出现异常的账户进行重新尝试"
-            Log(Message)
-            Pusher.PushToEnterpriseWechat(
-                "text", "manager", Message, Title="自动打卡")
+            PushToEnterpriseWechat(Message)
             for Account in RetryAccount:
                 ClockLine(Account)
         Finish()
+
     except Exception:
         ExceptionInformation = sys.exc_info()
         Message = "[运行异常]警告:主函数运行异常 已退出\n异常信息:{}".format(ExceptionInformation)
-        Log(Message)
-        Pusher.PushToEnterpriseWechat("text", "manager", Message, Title="自动打卡")
-        return 0
+        PushToEnterpriseWechat(Message)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
+    LogFile = ""  # 日志文件对象
+    AllAccount = []  # 队列中所有账户的数据
+    RetryAccount = []  # 队列中要重试的所有账户的数据
+    Headers = {
+        'Host': 'fangkong.hnu.edu.cn',  # 构造来源链接
+        'Content-Type': 'application/json',  # 重要
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+    }
+
+    # 企业微信Key
+    with open("Setting.json", "r", encoding="utf-8") as f:
+        Setting = json.load(f)
+    CorpID = Setting["EnterpriseWechat"]["CorpID"]
+    CorpSecret = Setting["EnterpriseWechat"]["CorpSecret"]
+    AgentID = Setting["EnterpriseWechat"]["AgentID"]
+    Manager = Setting["EnterpriseWechat"]["ManagerID"]
+    TokenApi = Setting["EnterpriseWechat"]["TokenApi"]
+    PushApi = Setting["EnterpriseWechat"]["PushApi"]
+
     Start()
